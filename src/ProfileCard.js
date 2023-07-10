3b4116a8c11ec57ce30eab7e933d4b493afd21fa
Engy <engeousthepotionmaster@gmail.com>
2023-07-10 20:06:02 +0300

    style cards

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,11 +1,21 @@
-function ProfileCard({title, handle, image}) {
-    return (
-        <div>
-            <img src={image} alt={`${title} logo`} />
-            <div>Title is {title}</div>
-            <div>Handle is {handle}</div>
+function ProfileCard({ title, handle, image }) {
+  return (
+    <div class="card">
+      <div class="card-image">
+        <figure class="image is-1by1">
+          <img src={image} alt={`${title} logo`} />
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">{title}</p>
+            <p class="subtitle is-6">{handle}</p>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default ProfileCard;
